0061bd060a7265c370d352d4272b791796a327cb
JooJooda <liyingzhou375@gmail.com>
2024-03-31 20:49:10 +0900

    feat : Comment 모델 생성

--- README.md
+++ README.md
@@ -12,3 +12,13 @@
 ----
 #### <center> Django-session 챌린지 과제 인증샷<center>
 ![스크린샷 2024-03-24 163210](https://github.com/LikeLion-at-CAU-12th/YoungJu-Lee/assets/150261503/aa284d67-3e7e-4f2a-bcbb-297fae2d4367)
+
+-------
+
+#### <center> DB-session 스탠다드 과제 인증샷 1 : ERD<center>
+![스크린샷 2024-03-31 201639](https://github.com/LikeLion-at-CAU-12th/YoungJu-Lee/assets/150261503/3c624b92-8aef-4867-8465-0bf355cb1212)
+
+#### <center> DB-session 스탠다드 과제 인증샷 2 : Admin 페이지 <center>
+![스크린샷 2024-03-31 204033](https://github.com/LikeLion-at-CAU-12th/YoungJu-Lee/assets/150261503/6864d9e4-c78c-4434-b6b7-120a3e77de5e)
+
+--------
--- django-session/posts/models.py
+++ django-session/posts/models.py
@@ -19,5 +19,12 @@ class Post(BaseModel):
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="제목", max_length=20)
     content = models.TextField(verbose_name="내용")
-    writer = models.CharField(verbose_name="작성자", max_length=10)
+    user_id = models.CharField(verbose_name="작성자", max_length=10)
     category = models.CharField(choices=CHOICES, max_length=20)
+
+class Comment(BaseModel):
+    id = models.IntegerField(primary_key=True)
+    comments = models.TextField(verbose_name="코멘트")
+    post_id = models.ForeignKey("Post", related_name="post", on_delete=models.CASCADE, db_column="post_id")
+    user_id = models.CharField(verbose_name="유저 아이디", max_length=10)
+    is_secret = models.BooleanField(verbose_name="비밀댓글", default=False)
