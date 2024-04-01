68df8f3b52bab3dd2541656e220e4b15d07c94a2
JooJooda <liyingzhou375@gmail.com>
2024-04-01 22:36:38 +0900

    fix : user_id 속성 자료형 수정

--- django-session/posts/models.py
+++ django-session/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import User
 
 ## 추상 클래스 정의
 class BaseModel(models.Model):
@@ -24,7 +25,7 @@ class Post(BaseModel):
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="제목", max_length=20)
     content = models.TextField(verbose_name="내용")
-    user_id = models.CharField(verbose_name="작성자", max_length=10)
+    user_id = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE, db_column="user_id")
     category = models.CharField(choices=CHOICES, max_length=20)
     hash_tag = models.ManyToManyField(HashTag, related_name='posts')
     
@@ -33,7 +34,7 @@ class Comment(BaseModel):
     id = models.AutoField(primary_key=True)
     comments = models.TextField(verbose_name="코멘트")
     post_id = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE, db_column="post_id")
-    user_id = models.CharField(verbose_name="유저 아이디", max_length=10)
+    user_id = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE, db_column="user_id")
     is_secret = models.BooleanField(verbose_name="비밀댓글", default=False)
     
 
