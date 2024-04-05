54854a3d2676aaef83cd56c317a70985214eeabe
JooJooda <liyingzhou375@gmail.com>
2024-04-05 19:44:50 +0900

    chore : CRUD 세션 복습

--- django-session/posts/urls.py
+++ django-session/posts/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name = 'my-page'),
-    path('introduction', printFriendData, name = 'printFriendData'),
-    path('<int:id>', get_post_detail, name = "게시글 조회"),
+    path('', post_list, name = "post_list"),
+    path('<int:id>', post_detail, name = "post_detail"),
+
 ]
